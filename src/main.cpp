a6fbb8f9146b7e2b9e34f347311f5ae84fce83a3
Dai <d0598299@fcu.edu.tw>
2018-10-05 19:58:31 +0800

    新增減法函式

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int main() {
     printf("Rule Breaker!!!\n");
     printf("5 + 2 = %d\n", addNumber(5, 2));
+    printf("5 - 2 = %d\n", subNumber(5, 2));
     printf("%d\n", getTriangleType(3, 4, 5));
     return 0;
 }
@@ -12,6 +13,10 @@ int main() {
 
 int addNumber(int a, int b) {
     return a + b;
+}
+
+int subNumber (int a,int b){
+    return a - b;
 }
 
 int getTriangleType(int a, int b, int c) {
