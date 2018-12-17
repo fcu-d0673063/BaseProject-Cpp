6cbe333c83a7bd109c5e73602145c83eb288b968
Dai <d0598299@fcu.edu.tw>
2018-12-17 20:23:20 +0800

    add Course and Evaluation

--- CourseEvaluationSystem/src/org/lasttaking/courseevaluationsystem/model/Course.java
+++ CourseEvaluationSystem/src/org/lasttaking/courseevaluationsystem/model/Course.java
@@ -5,5 +5,28 @@ package org.lasttaking.courseevaluationsystem.model;
  * @author D0656146
  */
 public class Course {
+	
+	private String id;
+    private String name;
+    private double rate;
+    
+    public Teacher[] teachers;
+    public Evaluation[] evaluations;
+    
+    public Course(String name){
+        this.name = name;
+    }
+    
+    public String getId(){
+        return id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public double getRate(){
+        return rate;
+    }
     
 }
--- CourseEvaluationSystem/src/org/lasttaking/courseevaluationsystem/model/Evaluation.java
+++ CourseEvaluationSystem/src/org/lasttaking/courseevaluationsystem/model/Evaluation.java
@@ -6,4 +6,34 @@ package org.lasttaking.courseevaluationsystem.model;
  */
 public class Evaluation {
     
+	private String id;
+    private int score;
+    private Teacher teacher;
+    private Course course;
+    private String comment;
+   
+    
+
+    
+    public String getId(){
+        return id;
+    }
+    
+    public int getScore(){
+        return score;
+    }
+    
+    public Teacher getTeacher(){
+        return teacher;
+    }
+    
+    public Course getCourse(){
+        return course;
+    }
+    
+    public String getComment(){
+        return comment;
+    }
+    
+	
 }
